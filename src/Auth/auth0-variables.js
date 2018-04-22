5c075cfff07d825b20698fcb4657c4eb5dc36606
C64\COMP64 <movel22@gmail.com>
2018-04-22 14:07:41 +0300

    add variable in heroku sever

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -7,5 +7,5 @@
 export const AUTH_CONFIG = {
   domain: 'movel.eu.auth0.com',
   clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
-  callbackUrl: 'https://login01-maxpfront.herokuapp.com/'
+  callbackUrl: 'https://login01-maxpfront.herokuapp.com/callback'
 }
