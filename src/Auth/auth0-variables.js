7b9f6e848df18d1f3b1c3ee8af6185335b182495
C64\COMP64 <movel22@gmail.com>
2018-04-22 14:00:51 +0300

    Change Callback to https://localhost:3000/callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,11 +1,11 @@
-export const AUTH_CONFIG = {
-  domain: 'movel.eu.auth0.com',
-  clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
-  callbackUrl: 'http://localhost:3000/callback'
-}
-
 // export const AUTH_CONFIG = {
 //   domain: 'movel.eu.auth0.com',
 //   clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
-//   callbackUrl: 'https://login01-maxpfront.herokuapp.com/callback'
+//   callbackUrl: 'http://localhost:3000/callback'
 // }
+
+export const AUTH_CONFIG = {
+  domain: 'movel.eu.auth0.com',
+  clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
+  callbackUrl: 'https://login01-maxpfront.herokuapp.com/'
+}
