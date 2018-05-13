95f873bf12154d797567da648020dfa0d9256841
C64\COMP64 <movel22@gmail.com>
2018-05-13 22:13:51 +0300

    small change in variables

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,11 +1,6 @@
-// export const AUTH_CONFIG = {
-//   domain: 'movel.eu.auth0.com',
-//   clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
-//   callbackUrl: 'http://localhost:3000/callback'
-// }
-
 export const AUTH_CONFIG = {
   domain: 'movel.eu.auth0.com',
   clientId: 'Q9Kc7WpojpAGHhduk2qhJe8yMQvI6K0l',
+  // callbackUrl: 'http://localhost:3000/callback',
   callbackUrl: 'https://test-react-redux-mcfee.herokuapp.com/callback'
 }
